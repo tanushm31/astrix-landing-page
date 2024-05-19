0b2f23f54f06a3828e72da711346c1f0c72d7f43
Tanush Mahajan <tanush.mohina@gmail.com>
2024-05-19 15:11:37 +0530

    Basic Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,111 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter, Krona_One, Lexend } from "next/font/google";
+import Marquee from "react-fast-marquee";
+// import Marquee from "./Marquee";
 
-const inter = Inter({ subsets: ["latin"] });
+const krona = Krona_One({
+	weight: "400",
+	variable: "--font-krona",
+	subsets: ["latin"],
+});
+const lexend = Krona_One({
+	weight: "400",
+	variable: "--font-lexend",
+	subsets: ["latin"],
+});
+
+const duplicateText = (text: string, count: number) => {
+	return new Array(count).fill(text).join(" * ");
+};
+
+type ISlide = {
+	id: number;
+	image: string; // image width = 412px height = 630 px
+	name: string;
+};
+
+const Slide = ({ image, name, id }: ISlide) => {
+	console.log("Image URL : ", `/Collections/${image}.png`);
+	const perc = 0.9;
+	// if (id === 1)
+	return (
+		<div className="flex flex-col justify-end h-full min-w-[412px] text-clip">
+			<div className={`text-3xl ${lexend.className} text-wrap pr-1 z-50`}>
+				{name} ft. Kanye West
+			</div>
+			<Image
+				src={`/Collections/${image}.png`}
+				alt={name}
+				height={630 * perc}
+				width={412 * perc}
+				// fill={}
+				className="object-cover"
+			/>
+		</div>
+	);
+};
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const bannerTexts = [
+		"Collection Live : Meta Lives , live on astrix",
+		"Event : Oasis Bus tour , JLN Stadium , Delhi ",
+	];
+	const colllectionsDetails = [
+		{ id: 1, image: "asset1", name: "Collection 1" },
+		{ id: 2, image: "asset2", name: "Collection 2" },
+		{ id: 3, image: "asset3", name: "Collection 3" },
+		{ id: 4, image: "asset4", name: "Collection 4" },
+	];
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	const eventDetails = [
+		{ id: 1, image: "asset1", name: "Event 1" },
+		{ id: 2, image: "asset2", name: "Event 2" },
+		{ id: 3, image: "asset3", name: "Event 3" },
+		{ id: 4, image: "asset4", name: "Event 4" },
+	];
+	return (
+		<main
+			className={`w-screen h-screen flex justify-between bg-matte-basic p-0`}
+		>
+			<div className="relative bg-matte-basic h-full w-[58%] flex flex-col justify-around ">
+				<div
+					className={`absolute top-30 z-10 w-[360px] h-[70%] font-extrabold text-white/20 text-6xl text-wrap flex justify-around flex-col ${lexend.className} pl-5`}
+				>
+					<div className="flex flex-col">
+						<span>ASTR</span>
+						<span>IX</span>
+					</div>
+					<div className="flex flex-col">
+						<span>COLL</span>
+						<span>ECTI</span>
+						<span>BLES</span>
+					</div>
+				</div>
+				<div className="z-30 flex overflow-x-scroll overflow-y-auto h-[90%] items-center">
+					{colllectionsDetails.map((collection) => {
+						return <Slide key={collection.name} {...collection} />;
+					})}
+					{/* <Slide {...colllectionsDetails[0]} /> */}
+				</div>
+			</div>
+			<div
+				className="bg-banner-purple h-full w-[6%] text-3xl font-bold text-white flex items-center justify-end leading-1 text-nowrap overflow-hidden"
+				style={{ writingMode: "vertical-rl" }}
+			>
+				{/* {bannerTexts.join(" * ")} */}
+				<div className={"vertical-marquee"}>
+					<div className={`marquee-content ${krona.className}`}>
+						{bannerTexts.join(" * ")}
+						{" * "}
+					</div>
+					<div className={`marquee-content ${krona.className}`}>
+						{" "}
+						{bannerTexts.join(" * ")}
+						{" * "}
+					</div>
+				</div>
+			</div>
+			<div className="bg-matte-basic h-full w-[36%]"></div>
+		</main>
+	);
 }
